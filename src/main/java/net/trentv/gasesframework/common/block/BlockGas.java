e9d7acd47e6247ea5e9a1e3d87cd31d19cce0e1b
trentv4 <trentvanslyke@gmail.com>
2017-02-13 21:02:38 -0500

    Remove gasblock drops from explosions

--- src/main/java/net/trentv/gasesframework/common/block/BlockGas.java
+++ src/main/java/net/trentv/gasesframework/common/block/BlockGas.java
@@ -1,5 +1,7 @@
 package net.trentv.gasesframework.common.block;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import javax.annotation.Nullable;
@@ -10,6 +12,7 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
@@ -19,7 +22,6 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import net.trentv.gasesframework.GasesFramework;
 import net.trentv.gasesframework.common.entity.EntityDelayedExplosion;
 import net.trentv.gasesframework.init.GasesFrameworkObjects;
 import net.trentv.gfapi.GFAPI;
@@ -44,6 +46,7 @@ public class BlockGas extends Block implements ISample
 		setHardness(0.0f);
 		setLightOpacity(type.opacity);
 		setCreativeTab(type.creativeTab);
+		setResistance(0);
 		setUnlocalizedName("gas_" + type.name);
 		this.setDefaultState(blockState.getBaseState().withProperty(CAPACITY, 16));
 	}
@@ -259,6 +262,12 @@ public class BlockGas extends Block implements ISample
     public Item getItemDropped(IBlockState state, Random rand, int fortune)
     {
         return null;
+    }
+    
+    public List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune)
+    {
+        List<ItemStack> ret = new ArrayList<ItemStack>();
+        return ret;
     }
     
 	public void onEntityCollidedWithBlock(World world, BlockPos pos, IBlockState state, Entity entity)
