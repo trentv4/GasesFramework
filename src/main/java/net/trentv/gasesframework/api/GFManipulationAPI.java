a4b3a4e5fff978587ca7662ed37519223c8904ab
trentv4 <trentvanslyke@gmail.com>
2017-10-20 09:32:26 -0400

    Implement addGasOverflow, which adds arbitrary gas to the world and creates a cloud of multiple blocks if necessary

--- src/main/java/net/trentv/gasesframework/api/GFManipulationAPI.java
+++ src/main/java/net/trentv/gasesframework/api/GFManipulationAPI.java
@@ -1,8 +1,11 @@
 package net.trentv.gasesframework.api;
 
+import java.util.ArrayList;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.trentv.gasesframework.GasesFramework;
@@ -130,5 +133,83 @@ public class GFManipulationAPI
 			GasesFramework.logger.error("Attempting to place an incorrect gas level of " + level);
 		}
 		return level;
+	}
+
+	public static void addGasLevelOverflow(BlockPos pos, World world, GasType gas, int level)
+	{
+		if (gas == GasesFramework.AIR)
+		{
+			return;
+		}
+		else if (level > 0)
+		{
+			if (canPlaceGas(pos, world, gas))
+			{
+				if (placeRecursive(pos, world, gas, level) == 0)
+				{
+					System.out.println("Finished placing");
+				}
+			}
+		}
+		else
+		{
+			GasesFramework.logger.error("Attempting to place an incorrect gas level of " + level);
+		}
+	}
+
+	private static int placeRecursive(BlockPos pos, World world, GasType gas, int originalLevel)
+	{
+		if (originalLevel == 0)
+			return 0;
+		if (!canPlaceGas(pos, world, gas))
+			return originalLevel;
+
+		int level = originalLevel;
+
+		if (originalLevel > 16)
+		{
+			level -= (16 + addGasLevel(pos, world, gas, 16));
+		}
+		else if (originalLevel < 16 & originalLevel > 0)
+		{
+			level -= (16 + addGasLevel(pos, world, gas, level));
+		}
+
+		EnumFacing[] faces = getValidFacings(pos, world, gas);
+		int[] distributedLevels = new int[faces.length];
+		int g = 0;
+		for (int i = level; i > 0; i--)
+		{
+			distributedLevels[g]++;
+			g++;
+			if (g >= distributedLevels.length)
+			{
+				g = 0;
+			}
+		}
+		int remaining = 0;
+		for (int i = 0; i < faces.length; i++)
+		{
+			if (distributedLevels[i] == 0)
+				return 0;
+			remaining += placeRecursive(pos.offset(faces[i]), world, gas, distributedLevels[i]);
+		}
+
+		return remaining;
+	}
+
+	private static EnumFacing[] getValidFacings(BlockPos pos, World world, GasType gas)
+	{
+		ArrayList<EnumFacing> validList = new ArrayList<EnumFacing>();
+
+		for (EnumFacing facing : EnumFacing.values())
+		{
+			if (canPlaceGas(pos, world, gas))
+			{
+				validList.add(facing);
+			}
+		}
+
+		return validList.toArray(new EnumFacing[validList.size()]);
 	}
 }
